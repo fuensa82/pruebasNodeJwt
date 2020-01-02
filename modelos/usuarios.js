93402ae33b025cac45c8e2c1e565374f48e0258a
fuensa82@gmail.com <fuensa82@gmail.com>
2020-01-02 14:58:25 +0100

    Pruebas con mysql

--- modelos/usuarios.js
+++ modelos/usuarios.js
@@ -1,13 +1,32 @@
 var mysql = require('mysql');
 
 function getListaUsuarios(req,res){
-    res.json({nombre:"Lista de usuarios"});
+    
+    conectar().connect(function(err){
+        if (err) throw err;
+        var sql="select idProfesor from profesores";// where nombreCorto='"+profesor.nombreCorto+"'";
+        var result=connect.query(sql, function (err, result, fields){
+            res.json({result});
+        });
+    });
+    
 }
 function getUsuario(req,res){
 
 }
 function crearUsuario(req,res){
 
+}
+
+function conectar(){
+    var con = mysql.createConnection({
+        host     : 'micasa82.ddns.net',
+        port     : '1106',
+        user     : 'vpalomo',
+        password : '123456',
+        database : 'colsan'
+    });
+    return con;
 }
 module.exports={
     getListaUsuarios,
